ba522ef841c6a42f34aab581fd9f6a1fb035bf38
Taddes Korris <taddes.korris@gmail.com>
2018-03-10 23:05:51 -0500

    initial commit of html, js and css files with layout

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,15 @@
+ 
+ 
+ 
+ 
+ // Initialize Firebase
+ var config = {
+    apiKey: "AIzaSyBM3mutqPNU3z3DRcw_pSBSb5kAnNOkG10",
+    authDomain: "fir-train-schedule-e3470.firebaseapp.com",
+    databaseURL: "https://fir-train-schedule-e3470.firebaseio.com",
+    projectId: "fir-train-schedule-e3470",
+    storageBucket: "fir-train-schedule-e3470.appspot.com",
+    messagingSenderId: "981369308911"
+  };
+  
+  firebase.initializeApp(config);
